c8c9da8bf4afef9b83e737ba30aa2ffd36ea9feb
Oğuzhan <unequaled86@gmail.com>
2019-06-28 20:00:44 +0300

    .

--- src/components/routes/oyun/index.js
+++ src/components/routes/oyun/index.js
@@ -131,7 +131,7 @@ export default class Oyun extends React.Component {
 
 	render() {
 		if (this.state.redirect) {
-			return <Redirect to=redirect />;
+			return <Redirect to={this.state.redirect} />;
 		}
 		return (
 			isLoggedIn() ? (
