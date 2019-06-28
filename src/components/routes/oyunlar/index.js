3395a6d194ee7257310f83e60ac9394495e628cc
Oğuzhan <unequaled86@gmail.com>
2019-06-28 20:35:38 +0300

    .

--- src/components/routes/oyunlar/index.js
+++ src/components/routes/oyunlar/index.js
@@ -25,7 +25,8 @@ export default class Oyunlar extends React.Component {
 		super(props);
 		this.state = {
 			ad: "",
-			oyunDialog: false
+			oyunDialog: false,
+			loading: false
 		};
     }
 
@@ -38,11 +39,12 @@ export default class Oyunlar extends React.Component {
 	}
 
 	getOyunlar = () => {
+		this.setState({loading: true})
 		getOyunlar().then((oyunlar) => {
 			/*oyunlar.map(oyun => (
 				this.props.addOyun(oyun)
 			));*/
-			this.setState({ oyunlar: oyunlar });
+			this.setState({ oyunlar: oyunlar, loading: false });
 		});
 	};
 
@@ -97,18 +99,24 @@ export default class Oyunlar extends React.Component {
 						<Button onClick={this.openOyunDialog}>oyun ekle</Button>
 					</Grid>
 					<Grid>
-						{this.state.oyunlar ? (
-							<Grid container alignItems="baseline" justify="space-evenly" direction="row" spacing={3}>
-							{this.state.oyunlar.map(oyun => (
-								<Grid item key={oyun.id} xs={6} md={3}>
-									<OyunItem key={oyun.id} oyun={oyun} onRemove={this.deleteOyun} />
-								</Grid>
-
-							))}
+						{this.state.loading ? (
+							<Grid container justify="center" direction="row" >
+								<Typography variant="p">Yükleniyor</Typography>
 							</Grid>
 						) : (
-							<Grid container justify="center" direction="row" >
-								<Typography variant="p">Burası Boş</Typography>
+							<Grid container alignItems="baseline" justify="space-evenly" direction="row" spacing={3}>
+							{this.state.oyunlar.length > 0 ? (
+								{this.state.oyunlar.map(oyun => (
+									<Grid item key={oyun.id} xs={6} md={3}>
+										<OyunItem key={oyun.id} oyun={oyun} onRemove={this.deleteOyun} />
+									</Grid>
+
+								))}
+							) : (
+								<Grid container justify="center" direction="row" >
+									<Typography variant="p">Burası Boş</Typography>
+								</Grid>
+							)}
 							</Grid>
 						)}
 					</Grid>
